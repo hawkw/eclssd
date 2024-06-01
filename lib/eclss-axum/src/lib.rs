b23f06ef0175b28c2f300cba10b6e4b83c886c58
Eliza Weisman <eliza@elizas.website>
2024-06-01 09:51:13 -0700

    feat(eclss-axum): more routes

--- lib/eclss-axum/src/lib.rs
+++ lib/eclss-axum/src/lib.rs
@@ -1,5 +1,11 @@
 pub use axum;
-use axum::{extract::State, routing::get, Json, Router};
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{Html, IntoResponse, Json},
+    routing::get,
+    Router,
+};
 use eclss::{sensor::Registry, Eclss, SensorMetrics};
 
 #[derive(Clone)]
@@ -11,11 +17,14 @@ struct AppState<const SENSORS: usize> {
 pub fn app<I, const SENSORS: usize>(eclss: &'static Eclss<I, { SENSORS }>) -> Router {
     Router::new()
         .route("/metrics", get(get_metrics))
+        .route("/metrics.json", get(get_metrics_json))
         .route("/sensors.json", get(get_sensors))
+        .route("/", get(index))
         .with_state(AppState {
             metrics: eclss.metrics(),
             sensors: eclss.sensors(),
         })
+        .fallback(not_found)
 }
 
 async fn get_metrics<const SENSORS: usize>(
@@ -26,8 +35,37 @@ async fn get_metrics<const SENSORS: usize>(
     resp
 }
 
+async fn get_metrics_json<const SENSORS: usize>(
+    State(AppState { metrics, .. }): State<AppState<{ SENSORS }>>,
+) -> Json<&'static SensorMetrics> {
+    Json(metrics)
+}
+
 async fn get_sensors<const SENSORS: usize>(
     State(AppState { sensors, .. }): State<AppState<{ SENSORS }>>,
 ) -> Json<&'static Registry<{ SENSORS }>> {
     Json(sensors)
+}
+
+async fn index() -> Html<&'static str> {
+    Html(
+        "<!DOCTYPE html>\
+        <html>\
+        <head>\
+            <title>ECLSS</title>\
+        </head>\
+        <body>\
+            <h1>ECLSS</h1>\
+            <ul>\
+                <li><a href=\"/metrics\">Metrics (Prometheus)</a></li>\
+                <li><a href=\"/metrics.json\">Metrics (JSON)</a></li>\
+                <li><a href=\"/sensors.json\">Sensors (JSON)</a></li>\
+            </ul>\
+        </body>\
+        </html>",
+    )
+}
+
+async fn not_found() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "can't get ye flask")
 }
