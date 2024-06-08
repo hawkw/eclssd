23d8a27fb5133f79d99e282b3a908aff52d5dc02
Eliza Weisman <eliza@elizas.website>
2024-06-08 14:13:51 -0700

    fix(eclssd): for real this time

--- eclssd/src/main.rs
+++ eclssd/src/main.rs
@@ -6,7 +6,7 @@ use embedded_hal::{
     delay::DelayNs as BlockingDelayNs,
     i2c::{self, I2c as BlockingI2c},
 };
-use embedded_hal_async::i2c::I2c;
+use embedded_hal_async::{delay::DelayNs, i2c::I2c};
 use linux_embedded_hal::I2cdev;
 use std::path::PathBuf;
 
