958e3288f619c1991cb8efff8bd359cd5854dd03
Eliza Weisman <eliza@elizas.website>
2024-06-16 13:01:43 -0700

    fix(sen5x): fix wrong i2c commands

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
             lockFile = ./Cargo.lock;
             outputHashes = {
               "linux-embedded-hal-0.4.0" = "sha256-2CxZcBMWaGP0DTiyQoDkwVFoNbEBojGySirSb2z+40U=";
-              "sensor-sen5x-0.1.0" = "sha256-T0nfojwrYXDYKgs6E/WGZpMBez1gTp8URkq2AEwxUMo=";
+              "sensor-sen5x-0.1.0" = "sha256-ufLHFpg4j7m8Ly3TVcB4u6r2XXZR3aHZkn6D0cAzlvk=";
               "sgp30-0.4.0" = "sha256-5LRVMFMTxpgQpxrN0sMTiedL4Sw6NBRd86+ZvRP8CVk=";
               "sht4x-0.2.0" = "sha256-LrOvkvNXFNL4EM9aAZfSDFM7zs6M54BGeixtDN5pFCo=";
               "sensirion-i2c-0.3.0" = "sha256-HS6anAmUBBrhlP/kBSv243ArnK3ULK8+0JA8kpe6LAk=";
